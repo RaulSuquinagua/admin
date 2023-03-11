a7b5790c5e4143caa784aff2ff5abd818ca2a7c8
Raul Suquinagua <raulsh2o@gmail.com>
2023-03-10 22:08:49 -0500

    first commit

---
+++ src/environments/environment.prod - back.ts
@@ -0,0 +1,14 @@
+export const environment = {
+  production: true,
+  adminEmail: 'user@gmail.com',
+  currency: '$',
+  firebase: {
+    apiKey: "AIzaSyBEf4CY1Qf5AV546v9lQpvhYn4evV-ek7I",
+    authDomain: "codeziporg.firebaseapp.com",
+    databaseURL: "https://codeziporg.firebaseio.com",
+    projectId: "codeziporg",
+    storageBucket: "codeziporg.appspot.com",
+    messagingSenderId: "305439070990",
+    appId: "1:305439070990:web:08225700f092aaec6d155f"
+  }
+};
